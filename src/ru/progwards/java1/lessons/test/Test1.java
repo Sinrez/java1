9589dc1417c77edbe8acd70365d1c880f7f8e789
sintez <Sin1984tez17>
2020-03-14 16:05:11 +0300

    hw3

---
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -0,0 +1,7 @@
+package ru.progwards.java1.lessons.test;
+
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+    }
+}
