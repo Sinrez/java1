925e2f7b7999f52629de437a6d59ad32c5f4a779
sintez <Sin1984tez17>
2020-03-23 22:25:12 +0300

    hw3 устранение ошибок после ревью

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -2,32 +2,33 @@ package ru.progwards.java1.lessons.basics;
 
     public class AccuracyDoubleFloat {
         double r = 6371.2;
+        private static final float fpi = 3.14f;
+        private static final double pi = 3.14;
 
 
         public static double volumeBallDouble(double radius){
             //4/3πR3.
-            double pi = 3.14;
             double v = (double)4 / 3 * pi * radius * radius * radius;
             return v;
         }
 
         public static float volumeBallFloat(float radius){
-            float pi = 3.14f;
-            float v = (float) (4 / 3 * radius * radius * radius);
+            float v = (float) 4 / 3 * fpi * radius * radius * radius;
             return v;
 
         }
 
-        public static double calculateAccuracy(double radius){
-        float rf = (float) radius;
-           double d = volumeBallDouble(radius);
-           float f = volumeBallFloat(rf);
-            return d - f;
+        public static double calculateAccuracy(double radius) {
+            return volumeBallDouble(radius) - volumeBallFloat((float) radius);
         }
+
 
 //        public static void main(String[] args) {
 //            double r = 6371.2;
+//            float fr =6371.2f;
 //            System.out.println(calculateAccuracy(r));
+//            System.out.println(volumeBallDouble(r));
+//            System.out.println(volumeBallFloat(fr));
 //        }
 
 }
