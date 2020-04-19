5de9855a1fdcb008f1cf627fc6affc59f32fe649
sintez <Sin1984tez17>
2020-04-19 17:12:44 +0300

    hw7 исправлен код для случая 0b00000001

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,20 +3,23 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
     //для двоичного числа 0100101 функция должна вернуть 3.
 
-    public static int sumBits(byte value){
-        {
-            int count = 0;
-
-            while (value != 0)
-            {
-                count += (value >>= 1) & 0b1;
-            }
-            return count;
+    public static int sumBits(byte value) {
+        // исправлен код для случая 0b00000001
+        byte r = 0;
+        if (value < 0) {
+            r++;
+            value &= 0b0111_1111;
         }
+        while (value != 0) {
+            r += value & 0b1;
+            value >>= 1;
+        }
+        return r;
     }
-//    public static void main(String[] args) {
-//        SumBits sumBits = new SumBits();
-//        System.out.println(sumBits.sumBits((byte)0b0010_0110));
-//
-//    }
+
+    public static void main(String[] args) {
+        SumBits sumBits = new SumBits();
+        System.out.println(sumBits.sumBits((byte)0b00000001));
+
+    }
 }
